d832095bc628dd1e85f49c169b01aae2ea7e147d
Slava65 <slava-sakh@yandex.ru>
2024-12-23 20:10:57 +0300

    pretier code

--- src/components/Preloader/Preloader.tsx
+++ src/components/Preloader/Preloader.tsx
@@ -1,8 +1,8 @@
-import preloaderImg from "../../assets/preloader.png"
+import preloaderImg from "../../assets/preloader.png";
 export default function Preloader() {
   return (
     <div className="preloader">
-        <img className="preloader__picture" src={preloaderImg}></img>;
+      <img className="preloader__picture" src={preloaderImg}></img>;
     </div>
-  )
+  );
 }
